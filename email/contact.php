a30fc9edf67c191147f97e66b6057573f661bb83
Levi Muniz <levi.muniz17@gmail.com>
2020-02-11 14:33:26 -0700

    WIP Added contact form
    WIP - Added contact form to contact us section

---
+++ email/contact.php
@@ -0,0 +1,41 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require '../../PHPMailer/src/Exception.php';
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
+
+$configs = include('../../config.php');
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.dreamhost.com';                   // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'contact@sebsscholarship.org';      // SMTP username
+    $mail->Password = $configs["password"];               // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('contact@sebsscholarship.org', 'Contact Form');      // Send from contact email address
+    $mail->addAddress('help@sebsscholarship.org');                      // Send to help list
+    $mail->addReplyTo($_POST["email"], $_POST["name"]);                 // Set reply-to to submitter's name
+    $mail->addCC($_POST["email"]);                                      // Send the submitter a copy
+
+    //Content
+    $mail->isHTML(false);                                               // Make sure plain-text is on
+    $mail->Subject = 'SEBS Scholarship Contact Form Submission';
+    $mail->Body = $_POST["message"];                                    // Message from form
+
+    $mail->send();
+    echo 'Message has been sent!';
+} catch (Exception $e) {
+    echo 'There was an error sending your message. Please email <a href="mailto:help@sebsscholarship.org">help@sebsscholarship.org</a> directly for assistance.';
+    //echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
