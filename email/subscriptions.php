92db68e1bdacdefcfbf60fd13bcbb0635d2b8911
Levi Muniz <levi.muniz17@gmail.com>
2020-02-14 20:03:03 -0700

    Changed logic to include different statuses
    CHANGED - Subscriptions php now has re-worked logic to account for various statuses

--- email/subscriptions.php
+++ email/subscriptions.php
@@ -1,5 +1,11 @@
 <?php
 
+abstract class STATUS {
+    const SUBSCRIBED = "subscribed";
+    const NOT_SUBSCRIBED = "unsubscribed";
+    const MISSING = "missing";
+}
+
 $config = include('../../config.php');
 $urlBase = "https://us4.api.mailchimp.com/3.0/lists/" . $config["listID"] . "/members/";
 
@@ -10,6 +16,41 @@ function userExists($userHash) {
     curl_setopt($ch, CURLOPT_MUTE, 1);
     curl_setopt($ch, CURLOPT_USERPWD, "user:" . $config["key"]);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+
+    if (!curl_errno($ch) && curl_getinfo($ch, CURLINFO_RESPONSE_CODE) === 200) {
+        $status = json_decode($response)["status"];
+        if ($status === "subscribed" || $status === "pending") {
+            curl_close($ch);
+            return STATUS::SUBSCRIBED;
+        } else {
+            curl_close($ch);
+            return STATUS::NOT_SUBSCRIBED;
+        }
+    } else {
+        curl_close($ch);
+        return STATUS::MISSING;
+    }
+}
+
+function updateUserStatus($userHash, $status) {
+    global $config, $urlBase;
+
+    $data = array(
+        'status' => $status
+    );
+    $jsonData = json_encode($data);
+
+    $ch = curl_init($urlBase . $userHash);
+    curl_setopt($ch, CURLOPT_MUTE, 1);
+    curl_setopt($ch, CURLOPT_USERPWD, "user:" . $config["key"]);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($jsonData))
+    );
     curl_exec($ch);
 
     if (!curl_errno($ch) && curl_getinfo($ch, CURLINFO_RESPONSE_CODE) === 200) {
@@ -22,7 +63,9 @@ function userExists($userHash) {
 }
 
 if (isset($_POST["sub"]) && isset($_POST["name"]) && isset($_POST["email"])) {
-    if (!userExists(md5($_POST["email"]))) {
+    $userHash = md5($_POST["email"]);
+    $status = userExists($userHash);
+    if ($status === STATUS::MISSING) {
         $data = array(
             'email_address' => $_POST["email"],
             'status' => 'pending',
@@ -50,13 +93,17 @@ if (isset($_POST["sub"]) && isset($_POST["name"]) && isset($_POST["email"])) {
         }
 
         curl_close($ch);
+    } else if ($status === STATUS::NOT_SUBSCRIBED) {
+        updateUserStatus($userHash, "pending");
+        echo '<META HTTP-EQUIV="refresh" content="0;URL=confirm.html">';
     } else {
         echo '<META HTTP-EQUIV="refresh" content="0;URL=subscribed.html">';
     }
 } else if (isset($_POST["unsub"]) && isset($_POST["email"])) {
     $userHash = md5($_POST["email"]);
+    $status = userExists($userHash);
 
-    if (userExists($userHash)) {
+    if ($status === STATUS::SUBSCRIBED) {
         $data = array(
             'status' => 'unsubscribed'
         );
@@ -78,7 +125,7 @@ if (isset($_POST["sub"]) && isset($_POST["name"]) && isset($_POST["email"])) {
         if (!curl_errno($ch) && curl_getinfo($ch, CURLINFO_RESPONSE_CODE) === 200) {
             echo '<META HTTP-EQUIV="refresh" content="0;URL=unsubscribed.html">';
         } else {
-            echo '<META HTTP-EQUIV="refresh" content="0;URL=invalid.html">';
+            echo '<META HTTP-EQUIV="refresh" content="0;URL=not-subscribed.html">';
         }
 
         curl_close($ch);
