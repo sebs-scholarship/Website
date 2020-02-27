4d3c3dfdd20910d5c4c607b859bf7cfe100e6073
Levi Muniz <levi.muniz17@gmail.com>
2020-02-27 11:55:22 -0700

    WIP - Fix subscriptions

--- email/subscriptions.php
+++ email/subscriptions.php
@@ -1,8 +1,5 @@
 <?php
 
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
 abstract class STATUS {
     const SUBSCRIBED = "subscribed";
     const NOT_SUBSCRIBED = "unsubscribed";
@@ -88,7 +85,7 @@ if (isset($_POST["g-recaptcha-response"]) && strlen($_POST["g-recaptcha-response
     }
 } else {
     http_response_code(400);
-    exit("<h1>Bad Request</h1>");
+    exit("<h1>Bad Request 1</h1>");
 }
 
 if (isset($_POST["sub"]) && isset($_POST["name"]) && strlen($_POST["name"]) > 0 && isset($_POST["email"])
@@ -140,5 +137,5 @@ if (isset($_POST["sub"]) && isset($_POST["name"]) && strlen($_POST["name"]) > 0 
     }
 } else {
     http_response_code(400);
-    exit("<h1>Bad Request</h1>");
+    exit("<h1>Bad Request 2</h1>");
 }
