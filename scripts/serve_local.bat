dccd405226d16f7c9ee06bd4f60f811a7699cbc9
Levi Muniz <levi.muniz17@gmail.com>
2026-06-22 15:27:45 -0600

    feat(build): Create custom docker image for building site

--- scripts/serve_local.bat
+++ scripts/serve_local.bat
@@ -1,7 +1,1 @@
-docker run -p 4000:4000 -it --rm ^
--v "./":/srv/jekyll -v "./_vendor/bundle":/usr/local/bundle -v "./_vendor/gem":/usr/gem ^
--e JEKYLL_UID=1001 -e JEKYLL_GID=1001 ^
-jekyll/builder:4.2.2 /bin/bash -c ^
-"chmod 777 /srv/jekyll ^
-&& chmod 777 /srv/jekyll/assets/images ^
-&& jekyll serve --future --drafts"
+docker compose up --build serve
